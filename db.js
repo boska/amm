d30aa4758d293c50f84e76a3ca2c793ef7cc0589
Boska <boska@roamandwander.com>
2014-04-02 17:23:57 +0800

    init

--- db.js
+++ db.js
@@ -1,17 +1,16 @@
-var mongoose = require( 'mongoose' );
-var Schema   = mongoose.Schema;
- 
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 var Ad = new Schema({
-    user_id    : String,
-    content    : String,
-    media_name : String,
-    company_name :String,
-    company_email:String,
-    company_phone:String,
-    company_url :String,
-    respond:String,
-    updated_at : Date
+  user_id: String,
+  content: String,
+  media_name: String,
+  company_name: String,
+  company_email: String,
+  company_phone: String,
+  company_url: String,
+  respond: String,
+  updated_at: Date
 });
- 
-mongoose.model( 'Ad', Ad );
-mongoose.connect( 'mongodb://localhost/express-ad' );
+mongoose.model('Ad', Ad);
+var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/express-ad';
+mongoose.connect('mongoUri');
